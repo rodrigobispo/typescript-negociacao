1329d978e5c6ea74c770cae67ee88e42555c8d36
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-03-15 13:35:42 -0300

    Simplificação de cod, modificador readonly, programação defensiva pra data

--- app/models/negociacoes.ts
+++ app/models/negociacoes.ts
@@ -3,11 +3,13 @@ import { Negociacao } from "./negociacao.js";
 export class Negociacoes {
     
     private negociacoes: Array<Negociacao> = [];
+    // private negociacoes: Negociacao[] = [];
 
     adiciona(negociacao: Negociacao): void {
         this.negociacoes.push(negociacao);
     }
 
+    // lista(): readonly Negociacao[] {
     lista(): ReadonlyArray<Negociacao> {
         return this.negociacoes;
     }
