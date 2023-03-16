f8ca1da3ad865a16ef4cb4b780fa05f2ec80f44c
rodrigobispo <rodrigoaugustocb@gmail.com>
2023-03-16 03:27:34 -0300

    Criação de decorators inspect e escape de javascript.

--- app/src/decorators/logar-tempo-de-execucao.ts
+++ app/src/decorators/logar-tempo-de-execucao.ts
@@ -1,17 +1,22 @@
 // esqueleto de um decorator
-export function logarTempoDeExecucao() {
+export function logarTempoDeExecucao(emSegundos: boolean = false) {
     return function(
         target: any,
         propertyKey: string,
         descriptor: PropertyDescriptor
     ) {
         const metodoOriginal = descriptor.value
-        
         descriptor.value = function(...args: Array<any>) {
+            let divisor = 1
+            let unidade = 'milisegundos'
+            if (emSegundos) {
+                divisor = 1000
+                unidade = 'segundos'
+            }
             const tempo1 = performance.now()
             const retorno = metodoOriginal.apply(this, args)
             const tempo2 = performance.now()
-            console.log(`${propertyKey}, tempo de execução: ${(tempo2 - tempo1)/1000} segundos.`)
+            console.log(`${propertyKey}, tempo de execução: ${(tempo2 - tempo1)/divisor} ${unidade}.`)
             retorno
         }
         return descriptor;
